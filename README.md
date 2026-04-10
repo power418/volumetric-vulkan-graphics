abb83794ddb55dd5d42309d218ebb09a113b370b
power418 <neovecpp@gmail.com>
2026-04-09 21:45:30 -0700

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# Volumetric Simulation in Skybox
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#define UNICODE
+#if defined(_WIN32) || defined(_WIN64)
+#include <Windows.h>
+#endif
+
+int main(int argc, char **argv) { return 0; }
